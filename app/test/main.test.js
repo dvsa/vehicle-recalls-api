7f954ff7bcb9ab9910040f294e59ed3e6c2cc2ca
Marcin-Duszynski <marcin@duszynski.pl>
2017-10-23 11:26:42 +0200

    e2e test cases

--- app/test/main.test.js
+++ app/test/main.test.js
@@ -17,13 +17,18 @@ describe('When recall check request was received', () => {
   describe('and it does not contain marque', () => {
     it('then 400 (Bad Request) http code is returned.');
   });
-  describe('and it contain vin and marque', () => {
-    it('and vehicle has outstanding recall then "Recall Outstanding" it is returned.', () => {
 
+  describe('and it contain vin and marque', () => {
+    it('and vehicle has outstanding recall then http 200 and message "Recall Outstanding" it is returned.', () => {
     });
 
-    it('and vehicle has not outstanding recall then "No Recall Outstanding" it is returned.', () => {
+    it('and vehicle has not outstanding recall then http code 200 and message "No Recall Outstanding" it is returned.', () => {
+    });
 
+    it('and SMMT is returning wrong VIN message then http code 403 with error message "Incorrect VIN"', () => {
+    });
+
+    it('and SMMT is returning wrong MARQUE message then http code 403 with error message "Incorrect MARQUE"', () => {
     });
   });
 });
