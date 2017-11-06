0d56bc730a520131f1f4f8ee7fbe0a08d504ac64
Marcin-Duszynski <marcin@duszynski.pl>
2017-11-06 09:09:22 +0100

    Log level fix

--- app/src/config/service.js
+++ app/src/config/service.js
@@ -3,5 +3,5 @@ exports.load = () => ({
   env: process.env.SERVICE_ENV || 'NotSet',
   functionName: process.env.AWS_LAMBDA_FUNCTION_NAME || 'NotSet',
   functionVersion: process.env.AWS_LAMBDA_FUNCTION_VERSION || 'NotSet',
-  logLevel: (process.env.RECALL_LOG_LEVEL || 'debug').toLowerCase(),
+  logLevel: (process.env.RECALL_LOG_LEVEL || 'error').toLowerCase(),
 });
--- app/src/logger/createLogger.js
+++ app/src/logger/createLogger.js
@@ -3,7 +3,7 @@ const serviceConfig = require('../config/service').load();
 
 module.exports.create = () => bunyan.createLogger({
   name: serviceConfig.name,
-  level: serviceConfig.level,
+  level: serviceConfig.logLevel,
   env: serviceConfig.env,
   functionName: serviceConfig.functionName,
   functionVersion: serviceConfig.functionVersion,
