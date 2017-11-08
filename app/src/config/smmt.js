7f2fe5c96b3f14e527bc1b42de4ab313094b625e
Marcin-Duszynski <marcin@duszynski.pl>
2017-11-08 08:07:33 +0100

    Loading smmt api key from KMS

--- app/src/config/smmt.js
+++ app/src/config/smmt.js
@@ -1,4 +1,26 @@
-exports.load = () => ({
-  smmtVincheckUri: process.env.SMMT_API_URI,
-  smmtApiKey: process.env.SMMT_API_KEY,
-});
+const AWS = require('aws-sdk');
+const loggerFactory = require('../logger/createLogger');
+
+const logger = loggerFactory.create();
+const kms = new AWS.KMS();
+
+let smmtApiKey;
+
+exports.load = () => {
+  if (!smmtApiKey) {
+    kms.decrypt({
+      CiphertextBlob: Buffer(process.env.SMMT_API_KEY, 'base64'),
+    }).promise()
+      .then((data) => {
+        smmtApiKey = data.Plaintext.toString('ascii');
+      })
+      .catch((reason) => {
+        logger.error(reason, 'SMMT api key decryption failed.');
+      });
+  }
+
+  return {
+    smmtVincheckUri: process.env.SMMT_API_URI,
+    smmtApiKey,
+  };
+};
